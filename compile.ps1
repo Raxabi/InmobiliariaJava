b92bc98f1eaa3751e84533653f86f55eaab5409a
Raxabi <beltrandiaztorres@gmail.com>
2023-04-26 14:22:48 +0200

    [ADD] Opcion añadida de ejecutar el archivo de modelo ademas de la compilacion usando una funcion y argumentos de linea de comandos

--- compile.ps1
+++ compile.ps1
@@ -1,9 +1,21 @@
-javac -d .\includes .\Propiedad\*.java
+param ($run)
+$class_path = ".\includes"
 
-javac -d .\includes .\Enumeraciones\*.java
+function compileForClassPath () {
+    javac.exe -d $class_path .\Propiedad\*.java
+    javac.exe -d $class_path .\Enumeraciones\*.java
+    javac.exe -d $class_path .\ClaseInmobiliaria\*.java
 
-javac -d .\includes .\ClaseInmobiliaria\*.java
+    #* Individual files
+    javac.exe -d $class_path .\InmobiliariaControlador.java
+    javac.exe -d $class_path .\InmobiliariaVista.java
 
-javac -d .\includes .\InmobiliariaControlador.java
+    if ($run) {
+        java.exe -cp $class_path .\InModelo.java
+    } else {
+        Write-Host "Puedes ejecutar tambien despues de la compilacion
+        pasandole '-run' como argumento de la linea de comandos"
+    }
+}
 
-javac -d .\includes .\InmobiliariaVista.java
+compileForClassPath
